3fe98a16f5226d5b105a3e1ef4dbb9ba4bacc47d
Christjacoben <jacobenchristopher@gmail.com>
2024-01-06 20:04:54 +0800

    fixing route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Forgot from "./components/Forgot";
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/mern-Ehilada-frontend/",
   build: {
     outDir: "build", // Specify the output directory
   },
