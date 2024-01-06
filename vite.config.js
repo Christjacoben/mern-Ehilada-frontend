89cf9ef14048d24ad6d8507e90b7428ee160db06
Christjacoben <jacobenchristopher@gmail.com>
2024-01-06 21:21:03 +0800

    remake dist

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
 
   build: {
-    outDir: "build", // Specify the output directory
+    outDir: "dist", // Specify the output directory
   },
 });
